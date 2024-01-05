f973a3cd6e75b6abbb46f23b8fa286e7a0aff51b
Rano Langari <langarirano@gmail.com>
2024-01-05 17:41:48 +0800

    add routes to update profile

--- routes/mahasiswaRoutes.js
+++ routes/mahasiswaRoutes.js
@@ -5,6 +5,7 @@ import {
   getHalfSkripsi,
   getSkripsiById,
   getProfile,
+  updateProfile,
 } from "../controller/mahasiswaController.js";
 import { isAuthorized } from "../utils/auth.js";
 import Express from "express";
@@ -16,6 +17,7 @@ Router.post("/login", loginMahasiswa);
 Router.post("/upload-skripsi", isAuthorized, uploadSkripsi);
 Router.get("/get-skripsi", isAuthorized, getHalfSkripsi);
 Router.get("/profile", isAuthorized, getProfile);
+Router.put("/profile", isAuthorized, updateProfile);
 Router.get("/detail-skripsi/:id", isAuthorized, getSkripsiById);
 
 export default Router;
