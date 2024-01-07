7bdc3ce3d9eb63a1a36a5425010dc3d0f2e849ae
Rano Langari <langarirano@gmail.com>
2024-01-08 03:03:37 +0800

    add routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,7 @@ import {
   createAdmin,
   loginAdmin,
   getAdmin,
+  checkLoginAdmin,
 } from "../controller/adminController.js";
 import { isAuthorized } from "../utils/auth.js";
 import Express from "express";
@@ -9,6 +10,7 @@ import Express from "express";
 const Router = Express.Router();
 
 Router.post("/login", loginAdmin);
+Router.get("/check-login", isAuthorized, checkLoginAdmin);
 Router.post("/create", createAdmin);
 Router.get("/getadmin", isAuthorized, getAdmin);
 
