d0d42eb87d4e2cefba61bca63cfd591b06be31b8
Rano Langari <langarirano@gmail.com>
2024-01-12 21:11:14 +0800

    add endpoint

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ import {
   loginAdmin,
   getAdmin,
   checkLoginAdmin,
+  getSkripsiProcess,
 } from "../controller/adminController.js";
 import { isAuthorized } from "../utils/auth.js";
 import Express from "express";
@@ -13,5 +14,5 @@ Router.post("/login", loginAdmin);
 Router.get("/check-login", isAuthorized, checkLoginAdmin);
 Router.post("/create", createAdmin);
 Router.get("/getadmin", isAuthorized, getAdmin);
-
+Router.get("/get-skripsi-process", isAuthorized, getSkripsiProcess);
 export default Router;
