ad7c4be38bc63ad99b3efef0b229c330f5a305ea
Rano Langari <langarirano@gmail.com>
2024-01-15 22:44:55 +0800

    -

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,7 @@ import {
   checkLoginAdmin,
   getSkripsiProcess,
   KonfirmasiSkripsi,
+  deleteSkripsi,
 } from "../controller/adminController.js";
 import { isAuthorized } from "../utils/auth.js";
 import Express from "express";
@@ -17,4 +18,5 @@ Router.post("/create", createAdmin);
 Router.get("/getadmin", isAuthorized, getAdmin);
 Router.get("/get-skripsi-process", isAuthorized, getSkripsiProcess);
 Router.put("/konfirmasi-skripsi/:id", isAuthorized, KonfirmasiSkripsi);
+Router.put("/delete-skripsi/:id", isAuthorized, deleteSkripsi);
 export default Router;
